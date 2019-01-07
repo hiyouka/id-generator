669327b0d1224c3612e3f66939696bfa2a45a003
hiyouka <2062578173@qq.com>
2019-01-07 16:09:07 +0800

    twitter generate ids

--- id-generator-service/src/main/java/com/jy/idgenerator/generator/SnowflakeIdGenerator.java
+++ id-generator-service/src/main/java/com/jy/idgenerator/generator/SnowflakeIdGenerator.java
@@ -10,8 +10,8 @@ package com.jy.idgenerator.generator;
  * 12位序列，毫秒内的计数，12位的计数顺序号支持每个节点每毫秒(同一机器，同一时间截)产生4096个ID序号
  * 加起来刚好64位，为一个Long型。
  * SnowFlake的优点是，整体上按照时间自增排序，并且整个分布式系统内不会产生ID碰撞(由数据中心ID和机器ID作区分)，并且效率较高，经测试，SnowFlake每秒能够产生26万ID左右。
- *
- * @author wangyifan on 17-3-16.
+ * @author jianglei
+ * @Date: 2019/1/7
  */
 public class SnowflakeIdGenerator {
     // ==============================Fields===========================================
